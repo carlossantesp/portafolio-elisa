b6f88fda32ec1f61a7dfe65f117f80c53ca20c8e
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-02-18 12:33:22 -0400

    Agregando el Info

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero";
+import Information from "../components/Information";
 import Layout from "../components/Layout";
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
+      <Information />
     </Layout>
   );
 }
