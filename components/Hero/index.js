aa76c3a2e26185d33a4716070c6b6ab91a77826d
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-05-03 23:44:43 -0400

    Actualizando el componente Hero

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,42 +1,15 @@
-import Link from "next/link";
-import Phone from "../../icons/phone";
-import Email from "../../icons/email";
 import styles from "./Hero.module.css";
 
-export default function Hero({ name, description, phone, email, imageMe }) {
+export default function Hero({ name, description, imageMe }) {
   return (
     <section className={styles.hero}>
-      <div className={styles.heroInformation}>
-        <span className={styles.heroSay}>¡Hola a todos!</span>
-        <h1 className={styles.heroTitle}>Soy {name}</h1>
-        <p className={styles.heroInfo}>{description}</p>
-        <ul className={styles.heroList}>
-          <li className={styles.heroItem}>
-            <Link href={`https://api.whatsapp.com/send?phone=${phone}`}>
-              <a className={styles.heroLink} target="_blank">
-                <Phone />
-                {phone}
-              </a>
-            </Link>
-          </li>
-          <li className={styles.heroItem}>
-            <Link href={`mailto:${email}`}>
-              <a className={styles.heroLink} target="_blank">
-                <Email />
-                {email}
-              </a>
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.heroImagen}>
-        <div className={styles.heroBox}>
-          <img src="/images/box-1.png" alt="Box green" />
-        </div>
-        <div className={styles.heroBox}>
-          <img src="/images/box-2.png" alt="Box purple" />
-        </div>
+      <div className={styles.image}>
         <img src={imageMe} alt={`Foto de ${name}`} />
+      </div>
+      <div className={styles.information}>
+        <span className={styles.say}>¡Hola a todos!</span>
+        <h1 className={styles.title}>Soy {name}</h1>
+        <p className={styles.description}>{description}</p>
       </div>
     </section>
   );
