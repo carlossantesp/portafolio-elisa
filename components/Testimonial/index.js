89efc227dba0fc6bf56e6e420c0cae786d46ed51
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-05-05 15:29:50 -0400

    Actualizando componente Testimonial

--- components/Testimonial/index.js
+++ components/Testimonial/index.js
@@ -4,16 +4,14 @@ export default function Testimonial({ testimonial }) {
   const { name, description, title, image } = testimonial;
   return (
     <div className={styles.testimonial}>
-      <div className={styles.gradient}>
-        <div className={styles.wrapper}>
-          <div className={styles.image}>
-            <img src={image} alt={name} />
-          </div>
-          <p className={styles.description}>"{description}"</p>
-          <p className={styles.title}>
-            {name} / {title}
-          </p>
+      <div className={styles.box}>
+        <div className={styles.image}>
+          <img src={image} alt={name} />
         </div>
+        <p className={styles.description}>"{description}"</p>
+        <p className={styles.title}>
+          {name} / {title}
+        </p>
       </div>
     </div>
   );
